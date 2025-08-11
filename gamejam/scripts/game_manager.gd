dd5ee7c5c5c4b51ac2127aa88fdd363263de9f83
ALInm2981 <alinm2981@gmail.com>
2025-08-11 16:26:46 +1000

    game logic finished

--- gamejam/scripts/game_manager.gd
+++ gamejam/scripts/game_manager.gd
@@ -1,28 +1,31 @@
 extends Node
 
+const TEST_PLAYER = preload("res://scenes/test_player.tscn")
+
+
 var playerCount = 0
 var startingDiceCount = 5
 var cups: Array
-var curBid = {"val": 0, "num": 0}
+var curBid: Dictionary = {"val": 0, "num": 0}
+var players: Array
+var que: Array
 
 
 func _ready() -> void:
 	add_player()
-	add_player()
-	add_player()
-	add_player()
-	print("we have ", playerCount, " players")
-	
+
 	initialize()
 	print("Starting rolls are", cups)
-	
-	shake_dice()
-	print("re roll           ", cups)
 
-	
 
 func add_player():
-	playerCount += 1
+	if(playerCount < 4):
+		var newPlayer = TEST_PLAYER.instantiate()
+		newPlayer.give_id(playerCount)
+		players.append(newPlayer)
+		que.append(newPlayer)
+		add_child(newPlayer)
+		playerCount += 1
 	
 func initialize():
 	#make a 2D array
@@ -32,18 +35,103 @@ func initialize():
 	for cup in cups:
 		for dice in startingDiceCount:
 			cup.append(randi_range(1,6))
+	for p in players:
+		p.update_dice(cups[p.playerId])
+	que[0].your_turn()
 
 #randomize players dice
 func shake_dice():
 	for cup in cups:
-		for n in range(0, cup.size() ):
+		for n in range(0, cup.size()):
 			cup[n] = randi_range(1,6)
+	for p in players:
+		p.update_dice(cups[p.playerId])
+		
+func next_round():
+	for p in players:
+		p.not_your_turn()
+	shake_dice()
 
+func next_turn():
+	que.append(que[0])
+	que.pop_front()
+	que[0].your_turn()
+	
+func whos_turn(playerId):
+	while(que[0].playerId != playerId):
+		que.append(que[0])
+		que.pop_front()
+		que[0].your_turn()
 	
 func reveal_all_dice():
 	#show all dica on table
 	pass
 	
 func check_for_winner():
-	#check if only one player remains
-	pass
+	if(que.size() == 1): # we have a winner
+		return true
+	else:
+		return false
+	
+func if_lost(playerId): #check ifa  player has lost all their dice if that is the case remove the player from the que
+	if(cups[playerId].size() == 0):
+		for n in range(0,que.size()):
+			if(que[n].playerId == playerId):
+				que[n].lost()
+				que.pop_at(n)
+		check_for_winner()
+		return true
+	else:
+		return false
+	
+func call_bs(playerId: int):
+	reveal_all_dice()
+	var count = 0
+	for cup in cups:
+		for dice in cup:
+			if(dice == curBid["val"]):
+				count += 1
+	if(count > curBid["num"]):
+		#wrong call
+		cups[playerId].pop_back()
+		if(!if_lost(playerId)):
+			whos_turn(playerId)
+	else:
+		#last guy lied
+		cups[que[-1].playerId].pop_back()
+		if_lost(que[-1].playerId)
+	next_round()
+	
+func spot_on(playerId: int):
+	reveal_all_dice()
+	var count = 0 
+	for cup in cups:
+		for dice in cup:
+			if(dice == curBid["val"]):
+				count += 1
+	if(count != curBid["num"]):
+		#wrong call lose a dice
+		cups[playerId].pop_back()
+		if(!if_lost(playerId)):
+			whos_turn(playerId)
+	else:
+		#spot on! every body lose a dice exept playerId
+		for p in que:
+			if(p.playerId != playerId):
+				cups[p.playerId].pop_back()
+				if_lost(p.playerId)
+	next_round()
+	
+func raise(playerId: int, bidNumber:int, bidValue:int):
+	if(((bidValue > curBid["val"] && bidNumber >= curBid["num"])||(bidValue >= curBid["val"] && bidNumber > curBid["num"])) and playerId == que[0].playerId):
+		curBid["val"] = bidValue
+		curBid["num"] = bidNumber
+		for p in players:
+			p.update_curBid(curBid)
+	next_turn()
+	next_round()
+	
+
+	
+
+	
--- gamejam/scripts/player.gd
+++ gamejam/scripts/player.gd
@@ -1,27 +1,79 @@
 extends Node
+class_name Player
 
 var dice: Array
-var curBid: Dictionary
+var curBid: Dictionary = {"val": 0, "num": 0}
 var myTurn: bool
+var playerId: int
+var bidNumber: int = 0
+var bidValue: int = 0
+
+func _ready() -> void:
+	print("Im player ", playerId ," and my dice are ", dice)
+
+func give_id(newPlayerId: int):
+	playerId = newPlayerId
 
 func update_dice(newDice: Array):
 	dice.assign(newDice)
 	
-func update_curBid(newBid: Dictionary):
-	curBid.assign(newBid)
-
 func check_dice():
 	print(dice)
+	
+func lost():
+	pass
+	
+func update_curBid(newBid: Dictionary):
+	curBid.assign(newBid)
+	$UI/Margin/Info_Panel/Latest_Bid/Current_Number.text = str(curBid["num"])
+	$UI/Margin/Info_Panel/Latest_Bid/Current_Value_Texture.tooltip_text = str(curBid["val"])
 
-func bid(bidVal ,bidNum):
-	if((bidVal > curBid["val"] && bidNum >= curBid["num"])||(bidVal >= curBid["val"] && bidNum > curBid["num"])):
-		pass
+func not_your_turn():
+	myTurn = false
 	
-func spot_on():
-	pass
+func your_turn():
+	myTurn = true
+	$UI/Margin/Controls_Box/Display.visible = myTurn
+	$UI/Margin/Controls_Box/Control_Buttons/Your_Turn.visible = myTurn
 	
-func call_lie():
-	pass
-	
-func this_player_turn():
-	pass
+
+func _on_increase_number_pressed() -> void:
+	bidNumber += 1
+	update_selected_display()
+
+func _on_decrease_number_pressed() -> void:
+	if(bidNumber > curBid["num"]):
+		bidNumber -= 1
+	else:
+		#make a error anim?
+		print("cant go lower than curBid")
+	update_selected_display()
+
+func _on_increase_value_pressed() -> void:
+	bidValue += 1
+	update_selected_display()
+
+func _on_decrease_value_pressed() -> void:
+	if(bidNumber > curBid["val"]):
+		bidValue -= 1
+	else:
+		#make a error anim?
+		print("cant go lower than curBid")
+	update_selected_display()
+
+func update_selected_display():
+	$UI/Margin/Controls_Box/Display/Selection_Display/Selected_Number.text = str(bidNumber)
+	$UI/Margin/Controls_Box/Display/Selection_Display/Selected_Value_Texture.tooltip_text = str(bidValue)
+
+
+func _on_lock_bid_pressed() -> void:
+	if((bidValue > curBid["val"] && bidNumber >= curBid["num"])||(bidValue >= curBid["val"] && bidNumber > curBid["num"])):
+		get_parent().raise(playerId, bidNumber, bidValue)
+
+
+func _on_call_pressed() -> void:
+	get_parent().call_bs(playerId)
+
+
+func _on_spot_on_pressed() -> void:
+	get_parent().spot_on(playerId)
