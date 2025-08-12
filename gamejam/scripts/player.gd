667df4a54dc8064255ef4a18500543ddaae35609
ALInm2981 <alinm2981@gmail.com>
2025-08-12 15:37:18 +1000

    ui upgrade ?

--- gamejam/scripts/player.gd
+++ gamejam/scripts/player.gd
@@ -9,35 +9,39 @@ var bidNumber: int = 0
 var bidValue: int = 1
 
 func _ready() -> void:
-	print("Im player ", playerId ," and my dice are ", dice)
+	pass
 
 func give_id(newPlayerId: int):
 	playerId = newPlayerId
 
 func update_dice(newDice: Array):
 	dice.assign(newDice)
-	print("Im player ", playerId ," and my dice are ", dice)
+	#print(dice)
 	
 	
 func check_dice():
-	print(dice)
+	#print(dice)
+	pass
 	
 func lost():
-	pass
+	notify("You have LOST this game restart to play again.")
 	
 func update_curBid(newBid: Dictionary):
 	curBid.assign(newBid)
 	bidNumber = curBid["num"]
 	bidValue = curBid["val"]
 	$UI/Margin/Info_Panel/Latest_Bid/Current_Number.text = str(curBid["num"])
-	$UI/Margin/Info_Panel/Latest_Bid/Current_Value_Texture.tooltip_text = str(curBid["val"])
+	$UI/Margin/Info_Panel/Latest_Bid/Container/Current_Value_Texture.frame = curBid["val"]-1
 	update_selected_display()
 
 
 func not_your_turn():
 	myTurn = false
+	$UI/Margin/Controls_Box/Display.visible = myTurn
+	$UI/Margin/Controls_Box/Control_Buttons/Your_Turn.visible = myTurn
 	
 func your_turn():
+	notify("It's your turn")
 	myTurn = true
 	$UI/Margin/Controls_Box/Display.visible = myTurn
 	$UI/Margin/Controls_Box/Control_Buttons/Your_Turn.visible = myTurn
@@ -52,14 +56,14 @@ func _on_decrease_number_pressed() -> void:
 		bidNumber -= 1
 	else:
 		#make a error anim?
-		print("cant go lower than curBid number")
+		notify(str("Can\'t go lower than Current bid # number"))
 	update_selected_display()
 
 func _on_increase_value_pressed() -> void:
 	if (bidValue < 6):
 		bidValue += 1
 	else:
-		print("cant go higher than 6")
+		notify(str("Can't go higher than 6 for 🎲 value"))
 	update_selected_display()
 
 func _on_decrease_value_pressed() -> void:
@@ -67,17 +71,17 @@ func _on_decrease_value_pressed() -> void:
 		bidValue -= 1
 	else:
 		#make a error anim?
-		print("cant go lower than curBid val")
+		notify(str("Can\'t go lower than Current bid 🎲 value"))
 	update_selected_display()
 
 func update_selected_display():
 	$UI/Margin/Controls_Box/Display/Selection_Display/Selected_Number.text = str(bidNumber)
-	$UI/Margin/Controls_Box/Display/Selection_Display/Selected_Value_Texture.tooltip_text = str(bidValue)
+	$UI/Margin/Controls_Box/Display/Selection_Display/Container/Selected_Value_Texture.frame = bidValue-1
 
 
 func _on_lock_bid_pressed() -> void:
 	if((bidValue > curBid["val"] && bidNumber >= curBid["num"])||(bidValue >= curBid["val"] && bidNumber > curBid["num"])):
-		print("you raise")
+		notify(str("You raised the bid to #", bidNumber,"   🎲", bidValue))
 		get_parent().raise(playerId, bidNumber, bidValue)
 
 
@@ -87,3 +91,10 @@ func _on_call_pressed() -> void:
 
 func _on_spot_on_pressed() -> void:
 	get_parent().spot_on(playerId)
+	
+func notify(str: String):
+	$UI/Margin/Notifications.text = str
+
+
+func _on_restart_pressed() -> void:
+	pass # Replace with function body.
